1c96a4cec5f86651a49d009d2dee9946d6224f72
mdHasif2001 <iamhasib727@gmail.com>
2022-03-26 21:57:46 +0600

    cart-section hasbeen touch

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import './Product.css';
 
 const Product = (props) => {
     const {name, img} = props.product;
+
     return (
         <div className='product'>
-            <img src={img} alt="" />
+            <img className='watch-img' src={img} alt="" />
             <p className='product-name'>{name}</p>
+           
         </div>
+        
     );
 };
 
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -13,7 +13,6 @@ const Shop = () => {
             }
 )
     }, []);
-        
 
     return (
         <div className='shop-container'>
